1f1538144414e76bd4f14312a29afd6684bd1920
hsmak <husain.alkhamis@gmail.com>
2020-12-13 20:43:05 -0600

    committing changes..

---
+++ _forks/pull_subtrees.sh
@@ -0,0 +1,6 @@
+
+## functional-programming-in-scala
+git subtree pull --prefix _forks/functional-programming-in-scala/ https://github.com/fpinscala/fpinscala.git master
+
+## learn-scala-programming
+git subtree pull --prefix _forks/learn-scala-programming/ https://github.com/PacktPublishing/Learn-Scala-Programming.git master
